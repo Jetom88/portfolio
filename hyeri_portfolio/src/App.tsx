e3a6702c010a5aca7ad9e6e13f998c5c20755f4c
Jetom88 <shinhro3o@gmail.com>
2025-01-20 12:25:43 +0900

    feat: reset, first section 추가

--- hyeri_portfolio/src/App.tsx
+++ hyeri_portfolio/src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <></>;
-}
+import FirstSection from "./_components/FirstSection";
+
+const App = () => {
+  return (
+    <>
+      <FirstSection />
+    </>
+  );
+};
 
 export default App;
---
+++ hyeri_portfolio/src/_components/FirstSection.tsx
@@ -0,0 +1,7 @@
+import "../style/firstSection.css";
+
+const FirstSection = () => {
+  return <div className="wrapper">협업과 성장에 집중하는 프론트엔드 개발자 신혜리입니다.</div>;
+};
+
+export default FirstSection;
