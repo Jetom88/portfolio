f937a4a5f036baea56d842faba6b61c22f4985f7
Jetom88 <shinhro3o@gmail.com>
2025-01-22 11:55:34 +0900

    feat: first section 추가

--- hyeri_portfolio/src/App.tsx
+++ hyeri_portfolio/src/App.tsx
@@ -1,8 +1,11 @@
+import "./_style/common.css";
+import Navigation from "./_components/common/Navigation";
 import FirstSection from "./_components/FirstSection";
 
 const App = () => {
   return (
     <>
+      <Navigation />
       <FirstSection />
     </>
   );
--- hyeri_portfolio/src/_components/FirstSection.tsx
+++ hyeri_portfolio/src/_components/FirstSection.tsx
@@ -1,7 +1,24 @@
-import "../style/firstSection.css";
+import "../_style/firstSection.css";
 
 const FirstSection = () => {
-  return <div className="wrapper">협업과 성장에 집중하는 프론트엔드 개발자 신혜리입니다.</div>;
+  return (
+    <div className="wrapper">
+      <h2 className="mainText">
+        <span className="bold">협업</span>과 <span className="bold">성장</span>에 집중하는 <br />
+        프론트엔드 개발자 <span className="bold">신혜리</span>입니다.
+      </h2>
+
+      <div className="mainImg">
+        <img src="/assets/main_img.png" alt="main img" />
+      </div>
+
+      <div className="scrollBox">
+        <div className="icon" />
+        <div className="icon" />
+        <div className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default FirstSection;
