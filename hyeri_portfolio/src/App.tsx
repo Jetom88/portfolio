13ec663e32c35adeab56faff247f2c6d11216908
Jetom88 <shinhro3o@gmail.com>
2025-02-08 18:10:03 +0900

    fix: meta tag를 index.html로 이동

--- hyeri_portfolio/src/App.tsx
+++ hyeri_portfolio/src/App.tsx
@@ -9,7 +9,6 @@ import SideProjectSection from "./_components/SideProjectSection";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const App = () => {
   const [navColor, setNavColor] = useState("white");
@@ -38,40 +37,6 @@ const App = () => {
 
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <title>프론트엔드 신혜리 포트폴리오</title>
-          <meta name="keywords" content="프론트엔드, 포트폴리오, 신혜리" />
-          <meta name="description" content="프론트엔드 신혜리의 포트폴리오입니다." />
-          <meta property="og:title" content="프론트엔드 신혜리 포트폴리오" />
-          <meta property="og:description" content="프론트엔드 신혜리의 포트폴리오입니다." />
-          <meta property="og:image" content="https://hyeri-portfolio.vercel.app/assets/opengraph.jpg" />
-          <meta property="og:url" content="https://hyeri-portfolio.vercel.app/" />
-          <link
-            rel="icon"
-            type="image/png"
-            href="https://hyeri-portfolio.vercel.app/assets/16x16.png"
-            sizes="16x16"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="https://hyeri-portfolio.vercel.app/assets/32x32.png"
-            sizes="32x32"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="https://hyeri-portfolio.vercel.app/assets/48x48.png"
-            sizes="48x48"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="https://hyeri-portfolio.vercel.app/assets/180x180.png"
-          />
-        </Helmet>
-      </HelmetProvider>
       <Navigation navColor={navColor} />
 
       <FirstSection />
