0c52487d8f14f49653323ff40816326de4496375
Jetom88 <shinhro3o@gmail.com>
2025-02-08 11:41:49 +0900

    feat: react-helmet-async 추가 / 메타 태그 추가

--- hyeri_portfolio/src/App.tsx
+++ hyeri_portfolio/src/App.tsx
@@ -9,6 +9,7 @@ import SideProjectSection from "./_components/SideProjectSection";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const App = () => {
   const [navColor, setNavColor] = useState("white");
@@ -37,6 +38,21 @@ const App = () => {
 
   return (
     <>
+      <HelmetProvider>
+        <Helmet>
+          <title>프론트엔드 신혜리 포트폴리오</title>
+          <meta name="keywords" content="프론트엔드, 포트폴리오, 신혜리" />
+          <meta name="description" content="프론트엔드 신혜리의 포트폴리오입니다." />
+          <meta property="og:title" content="프론트엔드 신혜리 포트폴리오" />
+          <meta property="og:description" content="프론트엔드 신혜리의 포트폴리오입니다." />
+          <meta property="og:image" content="이미지 URL" />
+          <meta property="og:url" content="/assets/opengraph.jpg" />
+          <link rel="icon" type="image/png" href="/assets/16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/assets/32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/assets/48x48.png" sizes="48x48" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/180x180.png" />
+        </Helmet>
+      </HelmetProvider>
       <Navigation navColor={navColor} />
 
       <FirstSection />
